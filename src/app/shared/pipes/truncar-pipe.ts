5bb1063fe9dff646c160c50b6ddb5b2cafea55fa
vitu1415 <vitoralves0801@gmail.com>
2025-10-12 20:24:50 -0300

    add truncar titulo

--- src/app/shared/pipes/truncar-pipe.ts
+++ src/app/shared/pipes/truncar-pipe.ts
@@ -6,8 +6,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class TruncarPipe implements PipeTransform {
 
   transform(descricao: string): string {
-    if (descricao.length > 20) {
-      return descricao.substring(0, 20) + '...';
+    if (descricao.length > 30) {
+      return descricao.substring(0, 30) + '...';
     } else {
       return descricao;
     }
